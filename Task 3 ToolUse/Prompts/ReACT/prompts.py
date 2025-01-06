0f15119fa5b48ee4066be7b5dace15ea772fe408
EthanWTL <wangtianlong1207@gmail.com>
2025-01-05 22:41:26 -0500

    task 3 add tool for clustering successfully, just need to finish up the pipeline

--- Task 3 ToolUse/Prompts/ReACT/prompts.py
+++ Task 3 ToolUse/Prompts/ReACT/prompts.py
@@ -1,6 +1,6 @@
 from langchain.prompts import PromptTemplate # type: ignore
 
-ZEROSHOT_REACT_INSTRUCTION = """Collect information for a query plan using interleaving 'Thought', 'Action', and 'Observation' steps. Ensure you gather valid information related to transportation, dining, attractions, and accommodation. All information should be written in Notebook, which will then be input into the Planner tool. Note that the nested use of tools is prohibited. 'Thought' can reason about the current situation, and 'Action' can have 3 different types:
+ZEROSHOT_REACT_INSTRUCTION = """Collect information for a query plan using interleaving 'Thought', 'Action', and 'Observation' steps. Ensure you gather valid information related to transportation, dining, attractions, and accommodation. All information should be written in Notebook, which will then be input into the Planner tool. Note that the nested use of tools is prohibited. Dont include phrases like "Action: ", "Action 5", "Thought 1", or "Thought: " in your response. 'Thought' can reason about the current situation, and 'Action' can have 5 different types:
 
 (1) AccommodationSearch[Budget,Preference]:
 Description: Find the accommodation that matches the preference.
@@ -23,8 +23,14 @@ Cuisine: The cuisine mentioned in the query.
 Preference: A list of preferences mentioned in the query.
 Example: RestaurantSearch[Expensive budget, Vietnamese, [Good Flavor, Good Value]] would return the expensive restaurants that offer Vietnamese cuisine, with good or excellent flavor and good or excellent value.
 
-(4) Planner[Query]
-Description: A smart planning tool that crafts detailed plans based on user input and the information stroed in Notebook.
+(4) BusinessClusterSearch[Number Of Clusters]:
+Description: A tool that finds the number of business clusters given the information that you've collected. The tool will choose what business to be considered and return their spatial clustering information.
+Parameters: 
+Number Of Clusters: Determine how many spatial clusters that you want to find. 
+Example: BusinessClusterSearch[3] would return you a list of 3 business clusters among some attractions and hotels that you've collected. The businesses in the same cluster indicates that they are closer to each other and prefered to be arranged for the same day of the travel.
+
+(5) Planner[Query]
+Description: A smart planning tool that crafts detailed plans based on user input and the information stored in Notebook.
 Parameters: 
 Query: The query from user.
 Example: Planner[Give me a 3-day trip plan in Philadelphia] would return a detailed 3-day trip plan.
