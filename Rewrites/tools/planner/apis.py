a5b6dece4a28f745cad0f84c9ca81c4a2aeb4d58
EthanWTL <wangtianlong1207@gmail.com>
2025-01-10 22:41:21 -0500

    finish code

--- Rewrites/tools/planner/apis.py
+++ Rewrites/tools/planner/apis.py
@@ -2,6 +2,7 @@ import os
 from langchain.prompts import PromptTemplate # type: ignore
 from Utils.Prompt import planner_route_OP_agent
 from langchain.chat_models import ChatOpenAI # type: ignore
+from langchain_mistralai import ChatMistralAI
 from langchain.schema import ( # type: ignore
     AIMessage,
     HumanMessage,
@@ -13,15 +14,23 @@ class Planner:
                 model_name: str = ''
                 ):
         OPENAI_API_KEY = os.getenv('OPEN_AI_API')
+        MISTRAL_API_KEY = os.getenv('MISTRAL_API_KEY')
         if model_name == 'gpt-4o-2024-11-20':
             self.llm = ChatOpenAI(model_name=model_name, temperature=0, max_tokens=15000, openai_api_key=OPENAI_API_KEY)
-        
+        if model_name == 'mistral-large-2411':
+            self.llm = ChatMistralAI(
+                model="mistral-large-2411",
+                temperature=0,
+                max_tokens=15000,
+                mistral_api_key = MISTRAL_API_KEY
+            )
+
         self.agent_prompt = agent_prompt
 
         pass
 
     def run(self, text, query):
-        return self.llm([HumanMessage(content=self._build_agent_prompt(text, query)) ]).content
+        return self.llm.invoke([HumanMessage(content=self._build_agent_prompt(text, query)) ]).content
     
     def _build_agent_prompt(self, text, query) -> str:
         return self.agent_prompt.format(
